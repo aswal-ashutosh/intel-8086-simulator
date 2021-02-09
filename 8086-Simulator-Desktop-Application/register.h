a59ba4b10be1bd69e64a9d3bd289f9e2719c19a4
ashu.aswal.333@gmail.com <ashu.aswal.333@gmail.com>
2021-02-09 21:29:39 +0530

    Replaced all registers with two map => REG8 & REG16

--- 8086-Simulator-Desktop-Application/register.h
+++ 8086-Simulator-Desktop-Application/register.h
@@ -1,64 +1,13 @@
 #pragma once
 #include"typedef.h"
+#include<unordered_map>
+
 class Register
 {
-	/*
-	<<<<<<<<<<<<<<CS REGISTER>>>>>>>>>>>>>>>>>>>>>
-	[1]CS holds the base address for the Code Segment.
-	[2]All Programs are stored in the Code Segment.
-	[3]It is multiplied by 10H, to give the 20-bit physical address of Code Segment.
-	Eg: If CS = 4321H then CS x 10H = 43210H will the starting address of Code Segment.
-	*/
-	static _16Bit _CS;
-
-	/*
-	<<<<<<<<<<<<<<<<<DS REGISTER>>>>>>>>>>>>>>>>>>>>>
-	[x]DS holds the base Address for the Data Segment.
-	[x]It is multiplid by 10H, to give the 20-bit physical address of the Data Segment.
-	Eg: If DS = 4321H then DS × 10H = 43210H è Starting address of Data Segment.
-	*/
-	static _16Bit _DS;
-
-	/*
-	<<<<<<<<<<<<<<<<<<<SS REGISTER>>>>>>>>>>>>>>>>>>>
-	[1]SS holds the base (Segment) address for the Stack Segment.
-	[2]It is multiplied by 10H (16d), to give the 20-bit physical address of the Stack Segment.
-	Eg: If SS = 4321H then SS × 10H = 43210H è Starting address of Stack Segment.
-	*/
-	static _16Bit _SS;
-
-	/*
-	<<<<<<<<<<<<<<<<<<ES REGISTER>>>>>>>>>>>>>>>>>>>>
-	[1]ES holds the base (Segment) address for the Extra Segment.
-	[2]It is multiplied by 10H (16d), to give the 20-bit physical address of the Extra Segment.
-	Eg: If ES = 4321H then ES × 10H = 43210H è Starting address of Extra Segment.
-	*/
-	static _16Bit _ES;
-
-	/*
-	<<<<<<<<<<<<<<<<<<INSTRUCTION POINTER>>>>>>>>>>>>>>>>
-	[1]It is a 16Bit Register.
-	[2]It holds offest to the next permutation in the Code Segment.
-	[3][IMPORTANT] Address of the next instruction is calculated as CS x 10H + IP.
-	[4]IP is incrementd after every instruction byte is fetched.
-	*/
 	static _16Bit _IP;
 
-	static _16Bit _AX; //AX Register(16-Bits)
-	static _16Bit _BX; //BX Register(16-Bits)
-	static _16Bit _CX; //CX Register(16-Bits)
-	static _16Bit _DX; //DX Register(16-Bits)
-
-	static Byte _AL, _AH; //AX Register as 2 8-Bit Registers
-	static Byte _BL, _BH; //BX Register as 2 8-Bit Registers
-	static Byte _CL, _CH; //CX Register as 2 8-Bit Registers
-	static Byte _DL, _DH; //DX Register as 2 8-Bit Registers
-
-
-	static _16Bit SP_;//Stack Pointer(16-Bits)
-	static _16Bit BP_;//Base Pointer(16-Bits)
-	static _16Bit SI_;//Source Index(16-Bits)
-	static _16Bit DI_;//Destination Index(16Bits)
+	static std::unordered_map<std::string, Byte> REG8;		//For 8-Bit Parts of 16-Bit Registers
+	static std::unordered_map<std::string, _16Bit> REG16;	//For 16-Bit Registers
 
 	//Function to Sync 8 Bit Register with 16Bit Register
 	static void Sync16BitWith8Bit(_16Bit&, const Byte&, const bool);	//When 8-Bit Get Changed, bool = true -> R8 are Higer 8-Bit else Lower 8-Bit
@@ -138,32 +87,36 @@ public:
 	void DH(const Byte&);
 };
 
-_16Bit Register::_CS;
-_16Bit Register::_DS;
-_16Bit Register::_SS;
-_16Bit Register::_ES;
+std::unordered_map<std::string, Byte> Register::REG8 =
+{
+	{REGISTER::AL, 0},
+	{REGISTER::BL, 0},
+	{REGISTER::CL, 0},
+	{REGISTER::DL, 0},
+	{REGISTER::AH, 0},
+	{REGISTER::BH, 0},
+	{REGISTER::CH, 0},
+	{REGISTER::DH, 0},
+};
 
+std::unordered_map<std::string, _16Bit> Register::REG16 =
+{
+	{REGISTER::CS, 0},
+	{REGISTER::DS, 0},
+	{REGISTER::SS, 0},
+	{REGISTER::ES, 0},
 
-_16Bit Register::_IP;
+	{REGISTER::AX, 0},
+	{REGISTER::BX, 0},
+	{REGISTER::CX, 0},
+	{REGISTER::DX, 0},
 
-_16Bit Register::_AX;
-_16Bit Register::_BX;
-_16Bit Register::_CX;
-_16Bit Register::_DX;
+	{REGISTER::SP, 0},
+	{REGISTER::BP, 0},
+	{REGISTER::DI, 0},
+	{REGISTER::SI, 0},
+};
 
-_16Bit Register::SP_;
-_16Bit Register::BP_;
-_16Bit Register::SI_;
-_16Bit Register::DI_;
-
-Byte Register::_AL;
-Byte Register::_BL;
-Byte Register::_CL;
-Byte Register::_DL;
-Byte Register::_AH;
-Byte Register::_BH;
-Byte Register::_CH;
-Byte Register::_DH;
 
 void Register::Sync16BitWith8Bit(_16Bit& R16, const Byte& R8, const bool high)
 {
@@ -215,61 +168,61 @@ void Register::Sync8BitWith16Bit(const _16Bit& R16, Byte& R8H, Byte& R8L)
 }
 
 /*GETTERS*/
-_16Bit Register::CS() { return _CS; }
-_16Bit Register::DS() { return _DS; }
-_16Bit Register::SS() { return _SS; }
-_16Bit Register::ES() { return _ES; }
+_16Bit Register::CS() { return REG16[REGISTER::CS]; }
+_16Bit Register::DS() { return REG16[REGISTER::DS]; }
+_16Bit Register::SS() { return REG16[REGISTER::SS]; }
+_16Bit Register::ES() { return REG16[REGISTER::ES]; }
 
 _16Bit Register::IP() { return _IP; }
 
-_16Bit Register::SP() { return SP_; }
-_16Bit Register::BP() { return BP_; }
-_16Bit Register::SI() { return SI_; }
-_16Bit Register::DI() { return DI_; }
+_16Bit Register::SP() { return REG16[REGISTER::SP]; }
+_16Bit Register::BP() { return REG16[REGISTER::BP]; }
+_16Bit Register::SI() { return REG16[REGISTER::SI]; }
+_16Bit Register::DI() { return REG16[REGISTER::DI]; }
 
-_16Bit Register::AX() { return _AX; }
-_16Bit Register::BX() { return _BX; }
-_16Bit Register::CX() { return _CX; }
-_16Bit Register::DX() { return _DX; }
+_16Bit Register::AX() { return REG16[REGISTER::AX]; }
+_16Bit Register::BX() { return REG16[REGISTER::BX]; }
+_16Bit Register::CX() { return REG16[REGISTER::CX]; }
+_16Bit Register::DX() { return REG16[REGISTER::DX]; }
 
-Byte Register::AL() { return _AL; }
-Byte Register::BL() { return _BL; }
-Byte Register::CL() { return _CL; }
-Byte Register::DL() { return _DL; }
+Byte Register::AL() { return REG8[REGISTER::AL]; }
+Byte Register::BL() { return REG8[REGISTER::BL]; }
+Byte Register::CL() { return REG8[REGISTER::CL]; }
+Byte Register::DL() { return REG8[REGISTER::DL]; }
 
-Byte Register::AH() { return _AH; }
-Byte Register::BH() { return _BH; }
-Byte Register::CH() { return _CH; }
-Byte Register::DH() { return _DH; }
+Byte Register::AH() { return REG8[REGISTER::AH]; }
+Byte Register::BH() { return REG8[REGISTER::BH]; }
+Byte Register::CH() { return REG8[REGISTER::CH]; }
+Byte Register::DH() { return REG8[REGISTER::DH]; }
 
 /*SETTERS*/
-void Register::CS(const _16Bit& data) { _CS = data; }
-void Register::DS(const _16Bit& data) { _DS = data; }
-void Register::SS(const _16Bit& data) { _SS = data; }
-void Register::ES(const _16Bit& data) { _ES = data; }
+void Register::CS(const _16Bit& data) { REG16[REGISTER::CS] = data; }
+void Register::DS(const _16Bit& data) { REG16[REGISTER::DS] = data; }
+void Register::SS(const _16Bit& data) { REG16[REGISTER::SS] = data; }
+void Register::ES(const _16Bit& data) { REG16[REGISTER::ES] = data; }
 
 //[TODO]::OVERFLOW CHECKER[May be required]
 void Register::IP_INC(const _16Bit& data) { _IP += data; }/*Function to increment IP*/
 void Register::IP(const _16Bit& data) { _IP = data; }
 
-void Register::SP(const _16Bit& data) { SP_ = data; }
-void Register::BP(const _16Bit& data) { BP_ = data; }
-void Register::SI(const _16Bit& data) { SI_ = data; }
-void Register::DI(const _16Bit& data) { DI_ = data; }
+void Register::SP(const _16Bit& data) { REG16[REGISTER::SP] = data; }
+void Register::BP(const _16Bit& data) { REG16[REGISTER::BP] = data; }
+void Register::SI(const _16Bit& data) { REG16[REGISTER::SI] = data; }
+void Register::DI(const _16Bit& data) { REG16[REGISTER::DI] = data; }
 
 //[TODO]::Sync 16Bit with 8Bit
 
-void Register::AX(const _16Bit& data) { _AX = data; Sync8BitWith16Bit(_AX, _AH, _AL); };
-void Register::BX(const _16Bit& data) { _BX = data; Sync8BitWith16Bit(_BX, _BH, _BL); };
-void Register::CX(const _16Bit& data) { _CX = data; Sync8BitWith16Bit(_CX, _CH, _CL); };
-void Register::DX(const _16Bit& data) { _DX = data; Sync8BitWith16Bit(_DX, _DH, _DL); };
+void Register::AX(const _16Bit& data) { REG16[REGISTER::AX] = data; Sync8BitWith16Bit(REG16[REGISTER::AX], REG8[REGISTER::AH], REG8[REGISTER::AL]); };
+void Register::BX(const _16Bit& data) { REG16[REGISTER::BX] = data; Sync8BitWith16Bit(REG16[REGISTER::BX], REG8[REGISTER::BH], REG8[REGISTER::BL]); };
+void Register::CX(const _16Bit& data) { REG16[REGISTER::CX] = data; Sync8BitWith16Bit(REG16[REGISTER::CX], REG8[REGISTER::CH], REG8[REGISTER::CL]); };
+void Register::DX(const _16Bit& data) { REG16[REGISTER::DX] = data; Sync8BitWith16Bit(REG16[REGISTER::DX], REG8[REGISTER::DH], REG8[REGISTER::DL]); };
 
-void Register::AL(const Byte& data) { _AL = data; Sync16BitWith8Bit(_AX, _AL, false); }
-void Register::BL(const Byte& data) { _BL = data; Sync16BitWith8Bit(_BX, _BL, false); }
-void Register::CL(const Byte& data) { _CL = data; Sync16BitWith8Bit(_CX, _CL, false); }
-void Register::DL(const Byte& data) { _DL = data; Sync16BitWith8Bit(_DX, _DL, false); }
+void Register::AL(const Byte& data) { REG8[REGISTER::AL] = data; Sync16BitWith8Bit(REG16[REGISTER::AX], REG8[REGISTER::AL], false); }
+void Register::BL(const Byte& data) { REG8[REGISTER::BL] = data; Sync16BitWith8Bit(REG16[REGISTER::BX], REG8[REGISTER::BL], false); }
+void Register::CL(const Byte& data) { REG8[REGISTER::CL] = data; Sync16BitWith8Bit(REG16[REGISTER::CX], REG8[REGISTER::CL], false); }
+void Register::DL(const Byte& data) { REG8[REGISTER::DL] = data; Sync16BitWith8Bit(REG16[REGISTER::DX], REG8[REGISTER::DL], false); }
 
-void Register::AH(const Byte& data) { _AH = data; Sync16BitWith8Bit(_AX, _AH, true); }
-void Register::BH(const Byte& data) { _BH = data; Sync16BitWith8Bit(_BX, _BH, true); }
-void Register::CH(const Byte& data) { _CH = data; Sync16BitWith8Bit(_CX, _CH, true); }
-void Register::DH(const Byte& data) { _DH = data; Sync16BitWith8Bit(_DX, _DH, true); }
+void Register::AH(const Byte& data) { REG8[REGISTER::AH] = data; Sync16BitWith8Bit(REG16[REGISTER::AX], REG8[REGISTER::AH], true); }
+void Register::BH(const Byte& data) { REG8[REGISTER::BH] = data; Sync16BitWith8Bit(REG16[REGISTER::BX], REG8[REGISTER::BH], true); }
+void Register::CH(const Byte& data) { REG8[REGISTER::CH] = data; Sync16BitWith8Bit(REG16[REGISTER::CX], REG8[REGISTER::CH], true); }
+void Register::DH(const Byte& data) { REG8[REGISTER::DH] = data; Sync16BitWith8Bit(REG16[REGISTER::DX], REG8[REGISTER::DH], true); }
