2da69b2abf95ebd2ed1c24fd57cdf8f855131572
ashu.aswal.333@gmail.com <ashu.aswal.333@gmail.com>
2021-02-13 15:52:52 +0530

    Added Flag Checks in Mnemonic::ADD

--- 8086-Simulator-Desktop-Application/8086.h
+++ 8086-Simulator-Desktop-Application/8086.h
@@ -194,7 +194,7 @@ bool Mnemonic::ADD_CASE_1(const std::string& OP1, const std::string& OP2)
 	_16Bit REG1 = X;
 	_16Bit REG2 = Y;
 	REG1 += REG2;
-	Register::REG8(OP1, REG1);
+	Register::REG8(OP1, REG1);//Extra Bits will automatically get truncated as we are assigning 16Bit to 8Bit
 	Register::UpdateFlags8Bit(X, Y, REG1);
 	return true;
 }
@@ -202,11 +202,13 @@ bool Mnemonic::ADD_CASE_1(const std::string& OP1, const std::string& OP2)
 bool Mnemonic::ADD_CASE_2(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-2] REG8, MEM*/
-	_16Bit REG = Register::REG8(OP1);
-	_16Bit MEM = Memory::Get8Bit(Memory::PhysicalAddress(OP2));
+	Byte X = Register::REG8(OP1);
+	Byte Y = Memory::Get8Bit(Memory::PhysicalAddress(OP2));
+	_16Bit REG = X;
+	_16Bit MEM = Y;
 	REG += MEM;
-	//[TODO: FLAG CHECKS]
-	Register::REG8(OP1, REG);
+	Register::REG8(OP1, REG);//Extra Bits will automatically get truncated as we are assigning 16Bit to 8Bit
+	Register::UpdateFlags8Bit(X, Y, REG);
 	return true;
 }
 
@@ -214,11 +216,13 @@ bool Mnemonic::ADD_CASE_3(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-3] MEM, REG8*/
 	int PAdd = Memory::PhysicalAddress(OP1);
-	_16Bit MEM = Memory::Get8Bit(PAdd);
-	_16Bit REG = Register::REG8(OP2);
+	Byte X = Memory::Get8Bit(PAdd);
+	Byte Y = Register::REG8(OP2);
+	_16Bit MEM = X;
+	_16Bit REG = Y;
 	MEM += REG;
-	//[TODO: FLAG CHECKS]
-	Memory::Set8Bit(PAdd, MEM);
+	Memory::Set8Bit(PAdd, MEM);//Extra Bits will automatically get truncated as we are assigning 16Bit to 8Bit
+	Register::UpdateFlags8Bit(X, Y, MEM);
 	return true;
 }
 
@@ -230,7 +234,7 @@ bool Mnemonic::ADD_CASE_4(const std::string& OP1, const std::string& OP2)
 	int REG1 = X;
 	int REG2 = Y;
 	REG1 += REG2;
-	Register::REG16(OP1, REG1);
+	Register::REG16(OP1, REG1);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
 	Register::UpdateFlags16Bit(X, Y, REG1);
 	return true;
 }
@@ -238,12 +242,13 @@ bool Mnemonic::ADD_CASE_4(const std::string& OP1, const std::string& OP2)
 bool Mnemonic::ADD_CASE_5(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-5] REG16, MEM*/
-	int REG = Register::REG16(OP1);
-	
-	int MEM = Memory::Get16Bit(Memory::PhysicalAddress(OP2));
+	_16Bit X = Register::REG16(OP1);
+	_16Bit Y = Memory::Get16Bit(Memory::PhysicalAddress(OP2));
+	int REG = X;
+	int MEM = Y;
 	REG += MEM;
-	//[TODO: FLAG CHECKS]
-	Register::REG16(OP1, REG);
+	Register::REG16(OP1, REG);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
+	Register::UpdateFlags16Bit(X, Y, REG);
 	return true;
 }
 
@@ -251,11 +256,13 @@ bool Mnemonic::ADD_CASE_6(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-6] MEM, REG16*/
 	int PAdd = Memory::PhysicalAddress(OP1);
-	int MEM = Memory::Get16Bit(PAdd);
-	int REG = Register::REG16(OP2);
+	_16Bit X = Memory::Get16Bit(PAdd);
+	_16Bit Y = Register::REG16(OP2);
+	int MEM = X;
+	int REG = Y;
 	MEM += REG;
-	//[TODO: FLAG CHECKS]
-	Memory::Set16Bit(PAdd, MEM);
+	Memory::Set16Bit(PAdd, MEM);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
+	Register::UpdateFlags16Bit(X, Y, MEM);
 	return true;
 }
 
@@ -263,11 +270,13 @@ bool Mnemonic::ADD_CASE_7(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-7] MEM, IMMD8*/
 	int PAdd = Memory::PhysicalAddress(OP1);
-	_16Bit MEM = Memory::Get8Bit(PAdd);
-	_16Bit IMMD8 = Converter::HexToDec(OP2);
+	Byte X = Memory::Get8Bit(PAdd);
+	Byte Y = Converter::HexToDec(OP2);
+	_16Bit MEM = X;
+	_16Bit IMMD8 = Y;
 	MEM += IMMD8;
-	//[TODO: FLAG CHECKS]
-	Memory::Set8Bit(PAdd, MEM);
+	Memory::Set8Bit(PAdd, MEM);//Extra Bits will automatically get truncated as we are assigning 16Bit to 8Bit
+	Register::UpdateFlags8Bit(X, Y, MEM);
 	return true;
 }
 
@@ -275,44 +284,54 @@ bool Mnemonic::ADD_CASE_8(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-8] MEM, IMMD16*/
 	int PAdd = Memory::PhysicalAddress(OP1);
-	int MEM = Memory::Get16Bit(PAdd);
-	int IMMD16 = Converter::HexToDec(OP2);
+	_16Bit X = Memory::Get16Bit(PAdd);
+	_16Bit Y = Converter::HexToDec(OP2);
+	int MEM = X;
+	int IMMD16 = Y;
 	MEM += IMMD16;
-	//[TODO: FLAG CHECKS]
-	Memory::Set16Bit(PAdd, MEM);
+	Memory::Set16Bit(PAdd, MEM);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
+	Register::UpdateFlags16Bit(X, Y, MEM);
 	return true;
 }
 
 bool Mnemonic::ADD_CASE_9(const std::string& OP1, const std::string& OP2)
 {
 	/*[CASE-9] REG8, IMMD8*/
-	_16Bit REG = Register::REG8(OP1);
-	_16Bit IMMD8 = Converter::HexToDec(OP2);
+	Byte X = Register::REG8(OP1);
+	Byte Y = Converter::HexToDec(OP2);
+	_16Bit REG = X;
+	_16Bit IMMD8 = Y;
 	REG += IMMD8;
-	//[TODO: FLAG CHECKS]
-	Register::REG8(OP1, REG);
+	Register::REG8(OP1, REG);//Extra Bits will automatically get truncated as we are assigning 16Bit to 8Bit
+	Register::UpdateFlags8Bit(X, Y, REG);
 	return true;
 }
 
 bool Mnemonic::ADD_CASE_10(const std::string& OP1, const std::string& OP2)
 {
 	/*CASE-10] REG16, IMMD16*/
-	int REG = Register::REG16(OP1);
-	int IMMD16 = Converter::HexToDec(OP2);
+	_16Bit X = Register::REG16(OP1);
+	_16Bit Y = Converter::HexToDec(OP2);
+	int REG = X;
+	int IMMD16 = Y;
 	REG += IMMD16;
-	//[TODO: FLAG CHECKS]
-	Register::REG16(OP1, REG);
+	Register::REG16(OP1, REG);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
+	Register::UpdateFlags16Bit(X, Y, REG);
 	return true;
 }
 
 bool Mnemonic::ADD_CASE_11(const std::string& OP1, const std::string& OP2)
 {
 	/*CASE-11] REG16, IMMD8*/
-	int REG = Register::REG16(OP1);
-	int IMMD8 = Converter::HexToDec(OP2);
+	_16Bit X = Register::REG16(OP1);
+	_16Bit Y = Converter::HexToDec(OP2);
+	int REG = X;
+	int IMMD8 = Y;
 	REG += IMMD8;
-	//[TODO: FLAG CHECKS]
-	Register::REG16(OP1, REG);
+	Register::REG16(OP1, REG);//Extra Bits will automatically get truncated as we are assigning 32Bit to 16Bit
+	//This 16 Bit operation will affect the aux flag
+	Register::SetFlag(Register::FLAG::AF, (X & 0x000f) + (Y & 0x000f) > 0x000f);//Explicitly Checking aur Auxillary Carry
+	Register::UpdateFlags16Bit(X, Y, REG);
 	return true;
 }
 
